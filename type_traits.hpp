3f1792b8bb6c231af6a1c49d4aad18de665b980a
Jonathan Marini <j.marini@ieee.org>
2011-05-25 14:17:51 -0400

    Cleaned up vector a little, removed support for decimal types.

--- type_traits.hpp
+++ type_traits.hpp
@@ -20,16 +20,8 @@
 #define EUBLIB_TYPE_TRAITS_HPP
 
 #include <type_traits>
-#ifdef EUCLIB_DECIMAL_TYPES
-#	include <decimal/decimal>
-#endif
 
 namespace euclib { namespace mpl {
-
-template<typename T>
-struct is_decimal {
-	enum { value = 0 };
-};
 
 struct inaccurate_tag { };
 struct accurate_tag { };
@@ -44,31 +36,7 @@ struct accuracy_traits<double> { typedef inaccurate_tag category_t; };
 template< >
 struct accuracy_traits<long double> { typedef inaccurate_tag category_t; };
 
-#ifdef EUCLIB_DECIMAL_TYPES
-
-template< >
-struct accuracy_traits<std::decimal::decimal32> { typedef accurate_tag category_t; };
-template< >
-struct accuracy_traits<std::decimal::decimal64> { typedef accurate_tag category_t; };
-template< >
-struct accuracy_traits<std::decimal::decimal128> { typedef accurate_tag category_t; };
-
-template<>
-struct is_decimal<std::decimal::decimal32> { enum { value = 1 }; };
-template<>
-struct is_decimal<std::decimal::decimal64> { enum { value = 1 }; };
-template<>
-struct is_decimal<std::decimal::decimal128> { enum { value = 1 }; };
-
-#endif
-
 } // End namespace mpl
-
-#ifdef EUCLIB_DECIMAL_TYPES
-typedef std::decimal::decimal32 decimal32;
-typedef std::decimal::decimal64 decimal64;
-typedef std::decimal::decimal128 decimal128;
-#endif
 
 } // End namespace euclib
 
--- vector.hpp
+++ vector.hpp
@@ -657,133 +657,11 @@ public:
 
 
 /*
-#include "euclib_math.hpp"
-#include "point.hpp"
-
-namespace euclib {
-
-template<typename T, std::size_t D>
-class vector : public point_base<T,D> {
-// Typedefs
-protected:
-
-	typedef point_base<T,D> base_t;
-
-
-public:
-
-	typedef T               value_t;
-	typedef T&              ref_t;
-	typedef const T&        const_ref_t;
-	typedef std::size_t     size_t;
-	typedef std::array<T,D> data_t;
-	typedef const T*        raw_data_t;
-
-// Constructors
-public:
-
-	vector( ) : base_t( ) { }
-	vector( const base_t& pt ) : base_t( pt ) { }
-	vector( base_t&& pt ) : base_t( std::forward<base_t>( pt ) ) { }
-	template<typename E>
-	vector( const expression_holder<E>& expr ) : base_t( expr ) { }
-	template<typename ... Args>
-	vector( T value, Args... values ) : base_t( value, values... ) { }
-
-
-// Methods
-public:
-
-	vector<T,D> normalize( ) const {
-		vector<T,D> result;
-		T len = length( );
-
-		std::transform( base_t::m_data.begin( ), base_t::m_data.end( ), result.begin( ),
-				[&len]( const T& t ) {
-					return t / len;
-				}
-		);
-
-		return result;
-	}
-
-	void normalize_in_place( ) {
-		T len = length( );
-
-		std::transform( base_t::m_data.begin( ), base_t::m_data.end( ), base_t::m_data.begin( ),
-				[&len]( const T& t ) {
-					return t / len;
-				}
-		);
-	}
-
-	T dot( const vector<T,D>& v ) const {
-		T sum = 0;
-		for( std::size_t i = 0; i < D; ++i ) {
-			sum += base_t::m_data[i] * v.m_data[i];
-		}
-		return sum;
-	}
-
-	inline T length( ) const { return sqrt( length_sq( ) ); }
-
-	inline T length_sq( ) const {
-		T len = 0;
-		for( std::size_t i = 0; i < D; ++i ) {
-			len += base_t::m_data[i] * base_t::m_data[i];
-			assert( len > base_t::m_data[i] ); // quick code to catch some overflows
-		}
-		return len;
-	}
-
-}; // End class vector<T,D>
-
-
 template<typename T, unsigned int D>
 T dot( const vector<T,D>& v1, const vector<T,D>& v2 ) { return v1.dot( v2 ); }
 
 
-template<typename T>
-class vector<T,2> : public point_base<T,2> {
-// Typedefs
-protected:
-
-	typedef point_base<T,2> base_t;
-
-
-// Constructors
-public:
-
-	vector( ) : base_t( ) { }
-	vector( const base_t& pt ) : base_t( pt ) { }
-	vector( base_t&& pt ) : base_t( std::forward<base_t>( pt ) ) { }
-	template<typename E>
-	vector( const expression_holder<E>& expr ) : base_t( expr ) { }
-	template<typename ... Args>
-	vector( T value, Args... values ) : base_t( value, values... ) { }
-
-
-// Methods
-public:
-
-	T&  x( ) { return base_t::m_data[0]; }
-	T&  y( ) { return base_t::m_data[1]; }
-	T   x( ) const { return base_t::m_data[0]; }
-	T   y( ) const { return base_t::m_data[1]; }
-
-	vector<T,2> normalize( ) const {
-		T len = length( );
-		return vector<T,2> {
-		    base_t::m_data[0] / len,
-		    base_t::m_data[1] / len
-		};
-	}
-
-	void normalize_in_place( ) {
-		T len = length( );
-		base_t::m_data[0] /= len;
-		base_t::m_data[1] /= len;
-	}
+vector2 {
 
 	T dot( const vector<T,2>& v ) const {
 		return base_t::m_data[0] * v.m_data[0] + base_t::m_data[1] * v.m_data[1];
@@ -793,30 +671,7 @@ public:
 		return base_t::m_data[0] * v.m_data[1] - base_t::m_data[1] * v.m_data[0];
 	}
 
-	inline T length( ) const { return sqrt( length_sq( ) ); }
-
-	inline T length_sq( ) const {
-		return base_t::m_data[0] * base_t::m_data[0] +
-		       base_t::m_data[1] * base_t::m_data[1];
-	}
-
-	const T* c_ptr( ) const { return base_t::m_data.data( ); }
-
-// Operators
-public:
-
-	bool operator == ( const vector<T,2>& v ) const {
-		T slope1, slope2;
-		slope1 = base_t::m_data[0] / base_t::m_data[1];
-		slope2 = v[0] / v[1];
-		return equal( slope1, slope2 );
-	}
-
-	bool operator != ( const vector<T,2>& v ) const {
-		return !(*this == v);
-	}
-
-}; // End class vector<T,2>
+} // end vector2
 
 
 template<typename T>
@@ -826,53 +681,7 @@ template<typename T>
 T dot( const vector<T,2>& v1, const vector<T,2>& v2 ) { return v1.dot( v2 ); }
 
 
-template<typename T>
-class vector<T,3> : public point_base<T,3> {
-// Typedefs
-protected:
-
-	typedef point_base<T,3> base_t;
-
-
-// Constructors
-public:
-
-	vector( ) : base_t( ) { }
-	vector( const base_t& pt ) : base_t( pt ) { }
-	vector( base_t&& pt ) : base_t( std::forward<base_t>( pt ) ) { }
-	template<typename E>
-	vector( const expression_holder<E>& expr ) : base_t( expr ) { }
-	vector( T x ) : base_t( x ) { }
-	vector( T x, T y ) : base_t( x, y ) { }
-	vector( T x, T y, T z ) : base_t( x, y, z ) { }
-
-
-// Methods
-public:
-
-	T&  x( ) { return base_t::m_data[0]; }
-	T&  y( ) { return base_t::m_data[1]; }
-	T&  z( ) { return base_t::m_data[2]; }
-	T   x( ) const { return base_t::m_data[0]; }
-	T   y( ) const { return base_t::m_data[1]; }
-	T   z( ) const { return base_t::m_data[2]; }
-
-	vector<T,3> normalize( ) const {
-		T len = length( );
-		return vector<T,3> {
-		    base_t::m_data[0] / len,
-		    base_t::m_data[1] / len,
-		    base_t::m_data[2] / len
-		};
-	}
-
-	void normalize_in_place( ) {
-		T len = length( );
-		base_t::m_data[0] /= len;
-		base_t::m_data[1] /= len;
-		base_t::m_data[2] /= len;
-	}
-
+vector3 {
 	T dot( const vector<T,3>& v ) const {
 		return base_t::m_data[0] * v.m_data[0] +
 		       base_t::m_data[1] * v.m_data[1] +
@@ -900,18 +709,7 @@ public:
 		return this->dot( v1.cross( v2 ) );
 	}
 
-	inline T length( ) const { return sqrt( length_sq( ) ); }
-
-	inline T length_sq( ) const {
-		return base_t::m_data[0] * base_t::m_data[0] +
-		       base_t::m_data[1] * base_t::m_data[1] +
-		       base_t::m_data[2] * base_t::m_data[2];
-	}
-
-	const T* c_ptr( ) const { return base_t::m_data.data( ); }
-
-}; // End class vector<T,3>
-
+} // end vector3
 
 // v1 x ( v2 x v3 )
 template<typename T>
@@ -928,87 +726,27 @@ template<typename T>
 T dot( const vector<T,3>& v1, const vector<T,3>& v2 ) { return v1.dot( v2 ); }
 
 
-template<typename T>
-class vector<T,4> : public point_base<T,4> {
-// Typedefs
-protected:
-
-	typedef point_base<T,4> base_t;
-
-
-// Constructors
-public:
-
-	vector( ) : base_t( ) { }
-	vector( const base_t& pt ) : base_t( pt ) { }
-	vector( base_t&& pt ) : base_t( std::forward<base_t>( pt ) ) { }
-	template<typename E>
-	vector( const expression_holder<E>& expr ) : base_t( expr ) { }
-	vector( T x ) : base_t( x ) { }
-	vector( T x, T y ) : base_t( x, y ) { }
-	vector( T x, T y, T z ) : base_t( x, y, z ) { }
-	vector( T x, T y, T z, T w ) : base_t( x, y, z, w ) { }
-
-
-// Methods
-public:
-
-	T&  x( ) { return base_t::m_data[0]; }
-	T&  y( ) { return base_t::m_data[1]; }
-	T&  z( ) { return base_t::m_data[2]; }
-	T&  w( ) { return base_t::m_data[3]; }
-	T   x( ) const { return base_t::m_data[0]; }
-	T   y( ) const { return base_t::m_data[1]; }
-	T   z( ) const { return base_t::m_data[2]; }
-	T   w( ) const { return base_t::m_data[3]; }
-
-	vector<T,4> normalize( ) const {
-		T len = length( );
-		return vector<T,4> {
-		    base_t::m_data[0] / len,
-		    base_t::m_data[1] / len,
-		    base_t::m_data[2] / len,
-		    base_t::m_data[3] / len
-		};
-	}
-
-	void normalize_in_place( ) {
-		T len = length( );
-		base_t::m_data[0] /= len;
-		base_t::m_data[1] /= len;
-		base_t::m_data[2] /= len;
-		base_t::m_data[3] /= len;
-	}
-
+vector4 {
 	T dot( const vector<T,4>& v ) const {
 		return base_t::m_data[0] * v.m_data[0] +
 		       base_t::m_data[1] * v.m_data[1] +
 		       base_t::m_data[2] * v.m_data[2] +
 		       base_t::m_data[3] * v.m_data[3];
 	}
-
-	inline T length( ) const { return sqrt( length_sq( ) ); }
-
-	inline T length_sq( ) const {
-		return base_t::m_data[0] * base_t::m_data[0] +
-		       base_t::m_data[1] * base_t::m_data[1] +
-		       base_t::m_data[2] * base_t::m_data[2] +
-		       base_t::m_data[3] * base_t::m_data[3];
-	}
-
-	const T* c_ptr( ) const { return base_t::m_data.data( ); }
-
-}; // End class vector<T,4>
-
+} // end vector4
 
 template<typename T>
 T dot( const vector<T,4>& v1, const vector<T,4>& v2 ) { return v1.dot( v2 ); }
-
+*/
 
 // Various typedefs to make usage easier
 typedef vector<float,2>       vector2f;
 typedef vector<float,3>       vector3f;
 typedef vector<float,4>       vector4f;
+
+typedef vector<float,2>       vector2;
+typedef vector<float,3>       vector3;
+typedef vector<float,4>       vector4;
 
 typedef vector<double,2>      vector2d;
 typedef vector<double,3>      vector3d;
@@ -1018,20 +756,6 @@ typedef vector<long double,2> vector2ld;
 typedef vector<long double,3> vector3ld;
 typedef vector<long double,4> vector4ld;
 
-#ifdef EUCLIB_DECIMAL_TYPES
-typedef vector<decimal32,2>   vector2d32;
-typedef vector<decimal32,3>   vector3d32;
-typedef vector<decimal32,4>   vector4d32;
-
-typedef vector<decimal64,2>   vector2d64;
-typedef vector<decimal64,3>   vector3d64;
-typedef vector<decimal64,4>   vector4d64;
-
-typedef vector<decimal128,2>  vector2d128;
-typedef vector<decimal128,3>  vector3d128;
-typedef vector<decimal128,4>  vector4d128;
-#endif
-*/
 }  // End namespace euclib
 
 #endif // EUCLIB_VECTOR_HPP
